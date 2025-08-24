93c4a29695efff667d4e422759986f60ab1d843e
oogwu <oogwu@tencent.com>
2025-08-24 16:37:30 +0800

    feat: 加入苹果生成逻辑

--- main.fnl
+++ main.fnl
@@ -2,19 +2,20 @@
 (global WIDTH 20)
 (global HEIGHT 10)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;; snake ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (local Snake {})
 
-(fn Snake.new [cls dir nodes speed]
+(fn Snake.new [cls dir body speed]
     (local obj (setmetatable
-                 {: dir : nodes : speed}
+                 {: dir : body : speed}
                  {:__index cls}))
     obj)
 
-(fn Snake.move [self delta]
-    (set self.nodes
-         (icollect [_ [x y] (ipairs self.nodes)]
-                   [(+ x (* (. self.dir 1) self.speed delta))
-                    (+ y (* (. self.dir 2) self.speed delta))])))
+(fn Snake.move [self dt]
+    (set self.body
+         (icollect [_ [x y] (ipairs self.body)]
+                   [(+ x (* (. self.dir 1) self.speed dt))
+                    (+ y (* (. self.dir 2) self.speed dt))])))
 
 (fn Snake.turn-left [self]
     (let [[x y] self.dir]
@@ -25,20 +26,44 @@
       (set self.dir [(- y) x])))
 
 (fn Snake.draw [self rect]
-    (each [_ [x y] (ipairs self.nodes)]
+    (each [_ [x y] (ipairs self.body)]
           (rect :fill
                 (* x NODE-LENGTH) (* y NODE-LENGTH)
                 NODE-LENGTH NODE-LENGTH)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;; /snake ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;; apple ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(local Apple {})
+
+(fn Apple.new [cls snake-body]
+    (var x (math.random 0 (- WIDTH 1)))
+    (var y (math.random 0 (- HEIGHT 1)))
+    (while (>
+            (length (icollect [_ [ix iy] (ipairs snake-body)]
+                              (if (and (= ix x) (= iy y))
+                                1)))
+            0)
+           (set x (math.random 0 (- WIDTH 1)))
+           (set y (math.random 0 (- HEIGHT 1))))
+    (setmetatable {:pos [x y]} {:__index cls}))
+;;;;;;;;;;;;;;;;;;;;;;;;;;; /apple ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (local snake (Snake:new [1 0] [[0 (/ HEIGHT 2)]] 2))
+
+(local apples [])
 
 (fn love.keypressed [key scancode isrepeat]
     (match key
            :j (snake:turn-left)
            :k (snake:turn-right)))
 
-(fn love.update [delta]
-    (snake:move delta))
+(fn love.update [dt]
+    (if (= 0 (length apples)) (table.insert apples (Apple:new snake.body)))
+    (snake:move dt))
 
 (fn love.draw []
+    (each [_ {:pos [x y]} (ipairs apples)]
+          (love.graphics.rectangle :fill
+                                   (* x NODE-LENGTH) (* y NODE-LENGTH)
+                                   NODE-LENGTH NODE-LENGTH))
     (snake:draw love.graphics.rectangle))
--- main.lua
+++ main.lua
@@ -2,25 +2,25 @@ __fnl_global__NODE_2dLENGTH = 15
 WIDTH = 20
 HEIGHT = 10
 local Snake = {}
-Snake.new = function(cls, dir, nodes, speed)
-  local obj = setmetatable({dir = dir, nodes = nodes, speed = speed}, {__index = cls})
+Snake.new = function(cls, dir, body, speed)
+  local obj = setmetatable({dir = dir, body = body, speed = speed}, {__index = cls})
   return obj
 end
-Snake.move = function(self, delta)
+Snake.move = function(self, dt)
   do
     local tbl_21_ = {}
     local i_22_ = 0
-    for _, _1_ in ipairs(self.nodes) do
+    for _, _1_ in ipairs(self.body) do
       local x = _1_[1]
       local y = _1_[2]
-      local val_23_ = {(x + (self.dir[1] * self.speed * delta)), (y + (self.dir[2] * self.speed * delta))}
+      local val_23_ = {(x + (self.dir[1] * self.speed * dt)), (y + (self.dir[2] * self.speed * dt))}
       if (nil ~= val_23_) then
         i_22_ = (i_22_ + 1)
         tbl_21_[i_22_] = val_23_
       else
       end
     end
-    self.nodes = tbl_21_
+    self.body = tbl_21_
   end
   return nil
 end
@@ -37,14 +37,47 @@ Snake["turn-right"] = function(self)
   return nil
 end
 Snake.draw = function(self, rect)
-  for _, _3_ in ipairs(self.nodes) do
+  for _, _3_ in ipairs(self.body) do
     local x = _3_[1]
     local y = _3_[2]
     rect("fill", (x * __fnl_global__NODE_2dLENGTH), (y * __fnl_global__NODE_2dLENGTH), __fnl_global__NODE_2dLENGTH, __fnl_global__NODE_2dLENGTH)
   end
   return nil
 end
+local Apple = {}
+Apple.new = function(cls, snake_body)
+  local x = math.random(0, (WIDTH - 1))
+  local y = math.random(0, (HEIGHT - 1))
+  while true do
+    local _4_
+    do
+      local tbl_21_ = {}
+      local i_22_ = 0
+      for _, _5_ in ipairs(snake_body) do
+        local ix = _5_[1]
+        local iy = _5_[2]
+        local val_23_
+        if ((ix == x) and (iy == y)) then
+          val_23_ = 1
+        else
+          val_23_ = nil
+        end
+        if (nil ~= val_23_) then
+          i_22_ = (i_22_ + 1)
+          tbl_21_[i_22_] = val_23_
+        else
+        end
+      end
+      _4_ = tbl_21_
+    end
+    if not (#_4_ > 0) then break end
+    x = math.random(0, (WIDTH - 1))
+    y = math.random(0, (HEIGHT - 1))
+  end
+  return setmetatable({pos = {x, y}}, {__index = cls})
+end
 local snake = Snake:new({1, 0}, {{0, (HEIGHT / 2)}}, 2)
+local apples = {}
 love.keypressed = function(key, scancode, isrepeat)
   if (key == "j") then
     return snake["turn-left"](snake)
@@ -54,10 +87,20 @@ love.keypressed = function(key, scancode, isrepeat)
     return nil
   end
 end
-love.update = function(delta)
-  return snake:move(delta)
+love.update = function(dt)
+  if (0 == #apples) then
+    table.insert(apples, Apple:new(snake.body))
+  else
+  end
+  return snake:move(dt)
 end
 love.draw = function()
+  for _, _10_ in ipairs(apples) do
+    local _each_11_ = _10_["pos"]
+    local x = _each_11_[1]
+    local y = _each_11_[2]
+    love.graphics.rectangle("fill", (x * __fnl_global__NODE_2dLENGTH), (y * __fnl_global__NODE_2dLENGTH), __fnl_global__NODE_2dLENGTH, __fnl_global__NODE_2dLENGTH)
+  end
   return snake:draw(love.graphics.rectangle)
 end
 return love.draw
