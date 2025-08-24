f9686ff550aa294776b8abd0cf607fcd34849482
oogwu <oogwu@tencent.com>
2025-08-24 21:25:04 +0800

    feat: 支持吃苹果

--- main.fnl
+++ main.fnl
@@ -2,6 +2,8 @@
 (global WIDTH 20)
 (global HEIGHT 10)
 
+(global STATE nil)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;; snake ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (local Snake {})
 
@@ -11,11 +13,18 @@
                  {:__index cls}))
     obj)
 
-(fn Snake.move [self dt]
-    (set self.body
-         (icollect [_ [x y] (ipairs self.body)]
-                   [(+ x (* (. self.dir 1) self.speed dt))
-                    (+ y (* (. self.dir 2) self.speed dt))])))
+(fn Snake.eat [self]
+    (let [{:body [[head-x head-y]] :dir [dir-x dir-y]} self
+          new-x (+ head-x dir-x)
+          new-y (+ head-y dir-y)]
+      (table.insert self.body 1 [new-x new-y])))
+
+(fn Snake.move [self]
+    (let [{:body [[head-x head-y]] :dir [dir-x dir-y]} self
+          new-x (+ head-x dir-x)
+          new-y (+ head-y dir-y)]
+      (table.insert self.body 1 [new-x new-y])
+      (table.remove self.body)))
 
 (fn Snake.turn-left [self]
     (let [[x y] self.dir]
@@ -48,9 +57,26 @@
     (setmetatable {:pos [x y]} {:__index cls}))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;; /apple ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(local snake (Snake:new [1 0] [[0 (/ HEIGHT 2)]] 2))
-
+(local snake (Snake:new [1 0] [[0 (/ HEIGHT 2)]] 0.5))
 (local apples [])
+
+(fn all-but-last [x]
+    (icollect [idx item (ipairs x)]
+              (if (< idx (length x))
+                item)))
+
+(fn predicate-type [[x y]]
+    (let [[{:pos [apple-x apple-y]}] apples
+          body (all-but-last snake.body)]
+      (if (or (< x 0)
+              (>= x WIDTH)
+              (< y 0)
+              (>= y HEIGHT)) :wall
+        (> (length (icollect [_ [ix iy] (ipairs body)] (if (and (= ix x) (= iy y)) 1))) 0) :body
+        (and (= x apple-x) (= y apple-y)) :apple
+        :else nil)))
+
+(var total-dt 0)
 
 (fn love.keypressed [key scancode isrepeat]
     (match key
@@ -58,8 +84,22 @@
            :k (snake:turn-right)))
 
 (fn love.update [dt]
+    (set total-dt (+ total-dt dt))
     (if (= 0 (length apples)) (table.insert apples (Apple:new snake.body)))
-    (snake:move dt))
+    (if (> total-dt snake.speed)
+      (do
+        (set total-dt (- total-dt snake.speed))
+        (let [{:body [[head-x head-y]] :dir [dir-x dir-y]} snake
+              new-x (+ head-x dir-x)
+              new-y (+ head-y dir-y)
+              type (predicate-type [new-x new-y])]
+          (match type
+                 :wall (set STATE :GameOver)
+                 :body (set STATE :GameOver)
+                 :apple (do (snake:eat) (table.remove apples))
+                 nil (snake:move)))))
+    (if (= STATE :GameOver)
+      (love.graphics.print "Game Over" 100 100)))
 
 (fn love.draw []
     (love.graphics.setColor 0.8 0.2 0.2)
--- main.lua
+++ main.lua
@@ -1,28 +1,36 @@
 __fnl_global__NODE_2dLENGTH = 15
 WIDTH = 20
 HEIGHT = 10
+STATE = nil
 local Snake = {}
 Snake.new = function(cls, dir, body, speed)
   local obj = setmetatable({dir = dir, body = body, speed = speed}, {__index = cls})
   return obj
 end
-Snake.move = function(self, dt)
-  do
-    local tbl_21_ = {}
-    local i_22_ = 0
-    for _, _1_ in ipairs(self.body) do
-      local x = _1_[1]
-      local y = _1_[2]
-      local val_23_ = {(x + (self.dir[1] * self.speed * dt)), (y + (self.dir[2] * self.speed * dt))}
-      if (nil ~= val_23_) then
-        i_22_ = (i_22_ + 1)
-        tbl_21_[i_22_] = val_23_
-      else
-      end
-    end
-    self.body = tbl_21_
-  end
-  return nil
+Snake.eat = function(self)
+  local _let_1_ = self["body"]
+  local _let_2_ = _let_1_[1]
+  local head_x = _let_2_[1]
+  local head_y = _let_2_[2]
+  local _let_3_ = self["dir"]
+  local dir_x = _let_3_[1]
+  local dir_y = _let_3_[2]
+  local new_x = (head_x + dir_x)
+  local new_y = (head_y + dir_y)
+  return table.insert(self.body, 1, {new_x, new_y})
+end
+Snake.move = function(self)
+  local _let_4_ = self["body"]
+  local _let_5_ = _let_4_[1]
+  local head_x = _let_5_[1]
+  local head_y = _let_5_[2]
+  local _let_6_ = self["dir"]
+  local dir_x = _let_6_[1]
+  local dir_y = _let_6_[2]
+  local new_x = (head_x + dir_x)
+  local new_y = (head_y + dir_y)
+  table.insert(self.body, 1, {new_x, new_y})
+  return table.remove(self.body)
 end
 Snake["turn-left"] = function(self)
   local x = self.dir[1]
@@ -37,9 +45,9 @@ Snake["turn-right"] = function(self)
   return nil
 end
 Snake.draw = function(self, rect)
-  for _, _3_ in ipairs(self.body) do
-    local x = _3_[1]
-    local y = _3_[2]
+  for _, _7_ in ipairs(self.body) do
+    local x = _7_[1]
+    local y = _7_[2]
     rect("fill", (x * __fnl_global__NODE_2dLENGTH), (y * __fnl_global__NODE_2dLENGTH), __fnl_global__NODE_2dLENGTH, __fnl_global__NODE_2dLENGTH)
   end
   return nil
@@ -49,13 +57,13 @@ Apple.new = function(cls, snake_body)
   local x = math.random(0, (WIDTH - 1))
   local y = math.random(0, (HEIGHT - 1))
   while true do
-    local _4_
+    local _8_
     do
       local tbl_21_ = {}
       local i_22_ = 0
-      for _, _5_ in ipairs(snake_body) do
-        local ix = _5_[1]
-        local iy = _5_[2]
+      for _, _9_ in ipairs(snake_body) do
+        local ix = _9_[1]
+        local iy = _9_[2]
         local val_23_
         if ((ix == x) and (iy == y)) then
           val_23_ = 1
@@ -68,16 +76,78 @@ Apple.new = function(cls, snake_body)
         else
         end
       end
-      _4_ = tbl_21_
+      _8_ = tbl_21_
     end
-    if not (#_4_ > 0) then break end
+    if not (#_8_ > 0) then break end
     x = math.random(0, (WIDTH - 1))
     y = math.random(0, (HEIGHT - 1))
   end
   return setmetatable({pos = {x, y}}, {__index = cls})
 end
-local snake = Snake:new({1, 0}, {{0, (HEIGHT / 2)}}, 2)
+local snake = Snake:new({1, 0}, {{0, (HEIGHT / 2)}}, 0.5)
 local apples = {}
+local function all_but_last(x)
+  local tbl_21_ = {}
+  local i_22_ = 0
+  for idx, item in ipairs(x) do
+    local val_23_
+    if (idx < #x) then
+      val_23_ = item
+    else
+      val_23_ = nil
+    end
+    if (nil ~= val_23_) then
+      i_22_ = (i_22_ + 1)
+      tbl_21_[i_22_] = val_23_
+    else
+    end
+  end
+  return tbl_21_
+end
+local function predicate_type(_14_)
+  local x = _14_[1]
+  local y = _14_[2]
+  local _let_15_ = apples[1]
+  local _let_16_ = _let_15_["pos"]
+  local apple_x = _let_16_[1]
+  local apple_y = _let_16_[2]
+  local body = all_but_last(snake.body)
+  if ((x < 0) or (x >= WIDTH) or (y < 0) or (y >= HEIGHT)) then
+    return "wall"
+  else
+    local _17_
+    do
+      local tbl_21_ = {}
+      local i_22_ = 0
+      for _, _18_ in ipairs(body) do
+        local ix = _18_[1]
+        local iy = _18_[2]
+        local val_23_
+        if ((ix == x) and (iy == y)) then
+          val_23_ = 1
+        else
+          val_23_ = nil
+        end
+        if (nil ~= val_23_) then
+          i_22_ = (i_22_ + 1)
+          tbl_21_[i_22_] = val_23_
+        else
+        end
+      end
+      _17_ = tbl_21_
+    end
+    if (#_17_ > 0) then
+      return "body"
+    elseif ((x == apple_x) and (y == apple_y)) then
+      return "apple"
+    elseif "else" then
+      return nil
+    else
+      return nil
+    end
+  end
+end
+local total_dt = 0
 love.keypressed = function(key, scancode, isrepeat)
   if (key == "j") then
     return snake["turn-left"](snake)
@@ -88,18 +158,48 @@ love.keypressed = function(key, scancode, isrepeat)
   end
 end
 love.update = function(dt)
+  total_dt = (total_dt + dt)
   if (0 == #apples) then
     table.insert(apples, Apple:new(snake.body))
   else
   end
-  return snake:move(dt)
+  if (total_dt > snake.speed) then
+    total_dt = (total_dt - snake.speed)
+    local _let_24_ = snake["body"]
+    local _let_25_ = _let_24_[1]
+    local head_x = _let_25_[1]
+    local head_y = _let_25_[2]
+    local _let_26_ = snake["dir"]
+    local dir_x = _let_26_[1]
+    local dir_y = _let_26_[2]
+    local new_x = (head_x + dir_x)
+    local new_y = (head_y + dir_y)
+    local type = predicate_type({new_x, new_y})
+    if (type == "wall") then
+      STATE = "GameOver"
+    elseif (type == "body") then
+      STATE = "GameOver"
+    elseif (type == "apple") then
+      snake:eat()
+      table.remove(apples)
+    elseif (type == nil) then
+      snake:move()
+    else
+    end
+  else
+  end
+  if (STATE == "GameOver") then
+    return love.graphics.print("Game Over", 100, 100)
+  else
+    return nil
+  end
 end
 love.draw = function()
   love.graphics.setColor(0.8, 0.2, 0.2)
-  for _, _10_ in ipairs(apples) do
-    local _each_11_ = _10_["pos"]
-    local x = _each_11_[1]
-    local y = _each_11_[2]
+  for _, _30_ in ipairs(apples) do
+    local _each_31_ = _30_["pos"]
+    local x = _each_31_[1]
+    local y = _each_31_[2]
     love.graphics.rectangle("fill", (x * __fnl_global__NODE_2dLENGTH), (y * __fnl_global__NODE_2dLENGTH), __fnl_global__NODE_2dLENGTH, __fnl_global__NODE_2dLENGTH)
   end
   love.graphics.setColor(0.2, 0.8, 0.2)
